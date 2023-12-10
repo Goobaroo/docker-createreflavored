d0b4572513f4feb155252bf1e25b2aa7ac328e22
Jason McLeod <jason@mcleods.me>
2023-12-10 03:49:51 -0500

    Update to version 6.44

--- launch.sh
+++ launch.sh
@@ -12,9 +12,9 @@ else
 	exit 9
 fi
 
-if ! [[ -f 'CRF-Server-Files-6.43.zip' ]]; then
+if ! [[ -f 'CRF-Server-Files-6.44.zip' ]]; then
 	rm -fr config kubejs libraries mods *SERVER.zip forge*.jar
-	curl -Lo 'CRF-Server-Files-6.43.zip' 'https://edge.forgecdn.net/files/4928/280/CRF-Server-Files-6.43.zip' && unzip -u -o 'CRF-Server-Files-6.43.zip' -d /data
+	curl -Lo 'CRF-Server-Files-6.44.zip' 'https://edge.forgecdn.net/files/4943/778/CRF-Server-Files-6.44.zip' && unzip -u -o 'CRF-Server-Files-6.44.zip' -d /data
   curl -Lo forge-installer.jar 'https://maven.minecraftforge.net/net/minecraftforge/forge/'${FORGE_VERSION}'/forge-'${FORGE_VERSION}'-installer.jar'
 	java -jar forge-installer.jar --installServer && rm -f forge-installer.jar
 fi
